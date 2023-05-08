995cdbc6aa052b050575da0b993101a68a40ebae
周上行Ryer <melaiiryer@gmail.com>
2023-05-08 19:43:19 +0800

    feat: integrate official api

--- api.py
+++ api.py
@@ -1,11 +1,14 @@
 import csv
 import requests
+import pickle
 from random import choice
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
 from time import time
+from datetime import datetime
 from copy import deepcopy
 
+<<<<<<< HEAD
 headers = {
     'Host': 'book.sfacg.com',
     'Connection': 'keep-alive',
@@ -21,48 +24,25 @@ headers = {
 cookies = {
     '.SFCommunity': '<token>', # replace <token> with your .SFCommunity cookie
 }
+=======
+'''
+Cookie 的 .SFCommunity 和 seession_APP 是必须的
+可从 Android 的 /data/data/com.sfacg/app_webview/Default/Cookie 获取
+其为 SQLite Database
+'''
+>>>>>>> a361a8b (feat: integrate official api)
 
-class ChapterItem:
-    _keyList = None
+with open('secrets.csv') as f:
+    __SECRETPOOL__ = [e for e in csv.DictReader(f)]
 
-    def __init__(self, novel, cid, fetchData=True):
-        if ChapterItem._keyList is None:
-            with open('keylist.csv') as f:
-                ChapterItem._keyList = [e for e in csv.DictReader(f)]
+__SFCOMMUNITY__ = 'FIXME: Cookie ".SFCommunity"'
+__SESSIONAPP__ = 'FIXME: Cookie "session_APP"'
 
-        self.novel = novel
-        self.chapterId = cid
-        self._mark_as_dirty()
-
-        if fetchData:
-            self.fetch()
-
-    def _mark_as_dirty(self):
-        self.novelId = None
-        self.volumeId = None
-        self.chapterName = None
-        self.pubTime = None
-        self.updateTime = None
-        self.isVip = None
-
-        self.content = None
-
-    def __str__(self):
-        return f"{'{'}'ChapterName': {self.chapterName}, 'ChapterID': {self.chapterId}{'}'}"
-
-    def __repr__(self):
-        return str(self)
-
-    def _getSignKey(self):
-        return choice(ChapterItem._keyList)
-
-    def _fetch(self):
-        global cookies
-
-        signkey = self._getSignKey()
+class api:
+    def headers():
+        signkey = choice(__SECRETPOOL__)
         SFSecurity = '&'.join(map(lambda e: f'{e[0]}={e[1]}', signkey.items()))
         appversion = '4.7.46(android;25)'
-
         headers = {
             'Accept-Charset': 'UTF-8',
             'Authorization': 'Basic YW5kcm9pZHVzZXI6MWEjJDUxLXl0Njk7KkFjdkBxeHE=',
@@ -70,75 +50,214 @@ class ChapterItem:
             'User-Agent': f'boluobao/{appversion}/TENCENT/{signkey["devicetoken"].lower()}',
             'SFSecurity': SFSecurity,
         }
+        return headers
 
-        url = f'https://api.sfacg.com/Chaps/{self.chapterId}'
+    def cookies():
+        cookies = {
+           '.SFCommunity': __SFCOMMUNITY__,
+            'session_APP': __SESSIONAPP__,
+        }
+        return cookies
+
+    '''
+    api-url: https://api.sfacg.com/users
+    params:
+        - expand: [verifyType | avatarFrame | widgets],...
+        - uids: <userid>,...
+    '''
+    def users(uids, expand=[]):
+        url = 'https://api.sfacg.com/users'
         resp = requests.get(
             url,
             params={
-                'expand': 'content,needFireMoney,originNeedFireMoney,tsukkomi,chatlines',
-                'autoOrder': False
+                'expand': ','.join(expand),
+                'uids': ','.join(map(str, uids)),
             },
-            headers=headers,
-            cookies=cookies,
+            headers=api.headers(),
         )
+        return resp.json()
 
-        return resp.json()['data']
-
-    def fetch(self):
-        if self.content is None:
-            resp = self._fetch()
-
-            self.novelId = resp['novelId']
-            self.volumeId = resp['volumeId']
-            self.chapterName = resp['title']
-            self.pubTime = resp['addTime']
-            self.updateTime = resp['updateTime']
-            self.isVip = resp['isVip']
-            self.content = resp['expand']['content']
-
-class Novel:
-    def __init__(self, nid):
-        self.novelId = nid
-        self._mark_as_dirty()
-
-    def _mark_as_dirty(self):
-        self.volumeCache = None
-
-    @property
-    def volumes(self):
-        if self.volumeCache is None:
-            url = 'https://book.sfacg.com/ajax/ashx/Common.ashx'
-            resp = requests.post(
-                url,
-                params={
-                    'op': 'getPlanNovelInfo',
-                },
-                data={
-                    'nid': self.novelId,
-                },
-                headers=headers
-            )
-            self.volumeCache = resp.json()['novel']['VolumeSet']
-        return deepcopy(self.volumeCache)
-
-    def chaptersOf(self, id):
-        volumeIdSet = map(lambda e: e['VolumeID'], self.volumes)
-        if id not in volumeIdSet:
-            if not (id >= 0 and id < len(self.volumes)):
-                raise IndexError('volume index out of range')
-            id = self.volumes[id]['VolumeID']
-        url = 'https://i.sfacg.com/ajax/ashx/GetChapterInfo.ashx'
-        resp = requests.post(
+    '''
+    api-url: 'https://api.sfacg.com/users/<user-id>
+    params:
+        - expand: [introduction | bigAvatar | avatar |
+            backgroundPic | fansNum | followNum |
+            followyou | youfollow | verifyType |
+            verifyInfo | avatarFrame | youblock |
+            widgets],...'
+    '''
+    def userInfo(uid, expand=[]):
+        url = f'https://api.sfacg.com/users/{uid}'
+        resp = requests.get(
             url,
             params={
-                'vid': id,
+                'expand': ','.join(expand),
             },
-            headers=headers
+            headers=api.headers(),
         )
-        result = sorted(resp.json(), key=lambda e: e['co'])
-        result = map(lambda e: {
-            'ChapterName': unquote(e['ct']),
-            'ChapterID': e['cid'],
-        }, result)
-        result = map(lambda e: ChapterItem(self, e['ChapterID'], fetchData=False), result)
-        return list(result)
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/user/signInfo
+    '''
+    def signInfo():
+        url = 'https://api.sfacg.com/user/signInfo'
+        resp = requests.get(
+            url,
+            headers=api.headers(),
+            cookies=api.cookies(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/Chaps/<chapterId>
+    params:
+        - expand: [content | needFireMoney | originNeedFireMoney |
+            tsukkomi | chatlines],...
+        - autoOrder: [true | false]
+    note: vip 章节获取 content 需要登录
+    '''
+    def chapter(chapterId, expand=[], autoOrder=False):
+        url = f'https://api.sfacg.com/Chaps/{chapterId}'
+        resp = requests.get(
+            url,
+            params={
+                'expand': ','.join(expand),
+                'autoOrder': autoOrder,
+            },
+            headers=api.headers(),
+            cookies=api.cookies(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/novels/<novelId>
+    params:
+        - expand: [chapterCount | bigBgBanner | bigNovelCover |
+            typeName | intro | fav |
+            ticket | pointCount | tags |
+            sysTags | signlevel | discount |
+            discountExpireDate | totalNeedFireMoney | rankinglist |
+            originTotalNeedFireMoney | firstchapter | latestchapter |
+            latestcommentdate | essaytag | auditCover |
+            preOrderInfo | customTag | topic |
+            unauditedCustomtag],...
+    '''
+    def novelInfo(novelId, expand=[]):
+        url = f'https://api.sfacg.com/novels/{novelId}'
+        resp = requests.get(
+            url,
+            params={
+                'expand': ','.join(expand),
+            },
+            headers=api.headers(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/novels/<novelId>/dirs
+    params:
+        - expand: [originNeedFireMoney],...
+    '''
+    def catalogueOf(novelId, expand=[]):
+        url = f'https://api.sfacg.com/novels/{novelId}/dirs'
+        resp = requests.get(
+            url,
+            params={
+                'expand': ','.join(expand),
+            },
+            headers=api.headers(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/user
+    params:
+        - expand: [vipInfo | welfareCoin | isRealNameAuth |
+            changeNickNameInfo | welfareMoney | redpacketCode |
+            useWelfaresys | usedRedpacketCode | hasOrderChapWithFireMoney |
+            hasUnlockChapWithAd | hasActiveUnlockChapWithAd | hasOrderedVipChaps |
+            hasPaidFirstTime]
+    note: 需要登录
+    '''
+    def user(expand=[]):
+        url = 'https://api.sfacg.com/user'
+        resp = requests.get(
+            url,
+            params={
+                'expand': ','.join(expand),
+            },
+            headers=api.headers(),
+            cookies=api.cookies(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/androidcfg
+    '''
+    def androidConfig():
+        url = 'https://api.sfacg.com/androidcfg'
+        resp = requests.get(
+            url,
+            headers=api.headers(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/authors/<authorId>/novels
+    '''
+    def novelsOf(authorId):
+        url = f'https://api.sfacg.com/authors/{authorId}/novels'
+        resp = requests.get(
+            url,
+            headers=api.headers(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/authors
+    params:
+        - authorId: <authorId>
+        - expand: [youfollow | fansNum]
+    note: 未提供 .SFCommunity 时 youfollow 恒为 false
+    '''
+    def authorInfo(authorId, expand=[]):
+        url = 'https://api.sfacg.com/authors'
+        resp = requests.get(
+            url,
+            params={
+                'authorId': authorId,
+                'expand': ','.join(expand),
+            },
+            headers=api.headers(),
+            cookies=api.cookies(),
+        )
+        return resp.json()
+
+    '''
+    api-url: https://api.sfacg.com/user/badge
+    params:
+        - vipDateTime: <vipDateTime>
+        - badgeAddDateTime: <badgeAddDateTime>
+        - channle: [Android | ...]
+        - userIdentifer: <deviceToken>
+    note: 时间为 ISO 格式，精确到秒
+    '''
+    def badge(
+        vipDateTime='',
+        badgeAddDateTime=datetime.now().isoformat(timespec='seconds'),
+        channel='Android',
+        userIdentifer=choice(__SECRETPOOL__)['devicetoken'].lower()
+    ):
+        url = 'https://api.sfacg.com/user/badge'
+        resp = requests.get(
+            url,
+            params={
+                'vipDateTime': vipDateTime,
+                'badgeAddDateTime': badgeAddDateTime,
+                'channel': channel,
+                'userIdentifer': userIdentifer,
+            },
+            headers=api.headers(),
+        )
+        return resp.json()
